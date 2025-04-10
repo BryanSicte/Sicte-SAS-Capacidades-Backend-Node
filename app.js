00050c28d9c17fc46226564f7c8c0917f5dc6682
Brayan Steven Castelblanco Utria <brayan.kastel@gmail.com>
2025-04-10 18:16:11 -0500

    se corrige problema de cors

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+
 require('dotenv').config();
 
 const app = express();
 const routes = require('./routes');
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
