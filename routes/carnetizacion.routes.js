1716bcac63e352308789d060b278615fc0319c20
BryanUtria <brayan.kastel@gmail.com>
2025-04-11 11:25:14 -0500

    se agrega endpoints de carnetizacion v3

--- routes/carnetizacion.routes.js
+++ routes/carnetizacion.routes.js
@@ -66,9 +66,14 @@ router.post('/crearRegistro', async (req, res) => {
     }
 });
 
+const uploadDir = path.join(__dirname, '..', 'temp');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+}
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'temp/');
+        cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname));
