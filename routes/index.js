b2bf98521740abc94472ab1e9586357d3869233b
Brayan Steven Castelblanco Utria <brayan.kastel@gmail.com>
2025-04-14 11:26:54 -0500

    se agrega reporte de material ferretero

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const usuariosRoutes = require('./usuarios.routes');
 const chatbotRoutes = require('./chatbot.routes');
 const carnetizacionRoutes = require('./carnetizacion.routes');
 const solicitudMaterialRoutes = require('./solicitudMaterial.routes');
+const reporteFerreteroRoutes = require('./reporteFerretero.routes');
 
 router.use('/usuarios', usuariosRoutes);
 router.use('/chatbot', chatbotRoutes);
 router.use('/carnetizacion', carnetizacionRoutes);
 router.use('/solicitudMaterial', solicitudMaterialRoutes);
+router.use('/reporteFerretero', reporteFerreteroRoutes);
 
 module.exports = router;
