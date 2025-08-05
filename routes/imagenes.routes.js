b7b286405a4d4cfcb5d3e79638c558d00c8921d4
BryanUtria <brayan.kastel@gmail.com>
2025-08-05 16:03:12 -0500

    Se filtra la carpeta de las imagenes de la encuesta v2

--- routes/imagenes.routes.js
+++ routes/imagenes.routes.js
@@ -12,7 +12,7 @@ router.get('/encuestas', async (req, res) => {
     try {
         const result = await cloudinary.api.resources({
             type: 'upload',
-            prefix: 'encuestas/',
+            prefix: 'Encuestas/',
             max_results: 100
         });
         res.json(result.resources);
