fa189839335cc954eb3a563065d639aa8c450b41
BryanUtria <brayan.kastel@gmail.com>
2025-08-14 17:21:59 -0500

    Se quita el limite para cargar archivo por json

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-
 require('dotenv').config();
 
 const app = express();
 const routes = require('./routes');
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 3030;
