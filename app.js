bb9121eda118edd7f4f77314e0054624e9453c7b
Brayan Steven Castelblanco Utria <brayan.kastel@gmail.com>
2025-12-02 11:56:01 -0500

    se agrega localhost:8081 sobre el cors

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(cors({
     origin: [
         'http://localhost:3000',
         'http://localhost:8080',
+        'http://localhost:8081',
         'https://sictepowergmail.github.io',
         'https://bryansicte.github.io',
         'https://sicte-sas-ccot.up.railway.app',
