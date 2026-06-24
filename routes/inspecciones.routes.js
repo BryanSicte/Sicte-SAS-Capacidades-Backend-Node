4c90ad7072d6d5130f1ff587297ea35ab1074a47
BryanUtria <brayan.kastel@gmail.com>
2026-06-24 12:29:18 -0500

    Se implementa los roles en la app de inspecciones

--- routes/inspecciones.routes.js
+++ routes/inspecciones.routes.js
@@ -2106,4 +2106,104 @@ router.put('/registro/:id', validarToken, upload.any(), async (req, res) => {
     }
 });
 
+router.post('/roles', validarToken, async (req, res) => {
+
+    const usuarioToken = req.validarToken?.usuario || null;
+
+    try {
+        const data = req.body;
+
+        if (!data || Object.keys(data).length === 0) {
+            await registrarHistorial({
+                nombreUsuario: usuarioToken?.nombre || 'No registrado',
+                cedulaUsuario: usuarioToken?.cedula || 'No registrado',
+                rolUsuario: usuarioToken?.rol || 'No registrado',
+                nivel: 'log',
+                plataforma: determinarPlataforma(req.headers['user-agent'] || ''),
+                app: 'inspecciones',
+                metodo: 'post',
+                endPoint: 'roles',
+                accion: 'Consulta registros fallido',
+                detalle: 'Los datos de usuario son requeridos.',
+                datos: { data },
+                tablasIdsAfectados: [],
+                ipAddress: getClientIp(req),
+                userAgent: req.headers['user-agent'] || ''
+            });
+
+            return sendError(res, 400, "Los datos de usuario son requeridos.");
+        }
+
+        if (!data?.cedula) {
+            await registrarHistorial({
+                nombreUsuario: usuarioToken.nombre || 'No registrado',
+                cedulaUsuario: usuarioToken.cedula || 'No registrado',
+                rolUsuario: usuarioToken.rol || 'No registrado',
+                nivel: 'log',
+                plataforma: determinarPlataforma(req.headers['user-agent'] || ''),
+                app: 'inspecciones',
+                metodo: 'post',
+                endPoint: 'roles',
+                accion: 'Consulta archivos fallido',
+                detalle: 'Se requiere la cedula para la consulta',
+                datos: { dataProporcionado: data },
+                tablasIdsAfectados: [],
+                ipAddress: getClientIp(req),
+                userAgent: req.headers['user-agent'] || ''
+            });
+
+            return sendError(res, 400, "Se requiere la cedula para la consulta");
+        }
+
+        const [rows] = await dbRailway.query('SELECT * FROM rol_inspecciones where cedula = ?', [data.cedula]);
+
+        await registrarHistorial({
+            nombreUsuario: usuarioToken?.nombre || 'No registrado',
+            cedulaUsuario: usuarioToken?.cedula || 'No registrado',
+            rolUsuario: usuarioToken?.rol || 'No registrado',
+            nivel: 'success',
+            plataforma: determinarPlataforma(req.headers['user-agent'] || ''),
+            app: 'inspecciones',
+            metodo: 'post',
+            endPoint: 'roles',
+            accion: 'Consulta registros exitosa',
+            detalle: `Se consultó ${rows.length} registros`,
+            datos: {},
+            tablasIdsAfectados: [],
+            ipAddress: getClientIp(req),
+            userAgent: req.headers['user-agent'] || ''
+        });
+
+        return sendResponse(
+            res,
+            200,
+            `Consulta exitosa`,
+            `Se obtuvieron ${rows.length} registros de roles en inspecciones.`,
+            rows
+        );
+    } catch (err) {
+        await registrarHistorial({
+            nombreUsuario: usuarioToken?.nombre || 'Error sistema',
+            cedulaUsuario: usuarioToken?.cedula || 'Error sistema',
+            rolUsuario: usuarioToken?.rol || 'Error sistema',
+            nivel: 'error',
+            plataforma: determinarPlataforma(req.headers['user-agent'] || ''),
+            app: 'inspecciones',
+            metodo: 'post',
+            endPoint: 'roles',
+            accion: 'Error al obtener los registros',
+            detalle: 'Error interno del servidor',
+            datos: {
+                error: err.message,
+                stack: process.env.NODE_ENV === 'development' ? err.stack : undefined
+            },
+            tablasIdsAfectados: [],
+            ipAddress: getClientIp(req),
+            userAgent: req.headers['user-agent'] || ''
+        });
+
+        return sendError(res, 500, "Error inesperado.", err);
+    }
+});
+
 module.exports = router;
